0aa362070920bb940eb5c5a74104c9b9869a1066
dannyid <danielshekhtman@gmail.com>
2015-07-10 22:11:20 -0400

    Move functions back inside message function so capture all fonts/images

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -2,32 +2,31 @@ import $ from 'jquery';
 import {completeImageUrl, reduceColorsAndFonts} from './modules/utils';
 
 $(() => {
-  // Get fonts and colors on page load
-  const elements = $.makeArray($('body *').not('script, link, style'));
-  const images = $.makeArray($('body img'));
-
-  let reduced = reduceColorsAndFonts(elements);
-
-  // Sort fonts in alphabetical order
-  reduced.results.allFonts.sort((a, b) => {
-    return a.toLowerCase().localeCompare(b.toLowerCase());
-  });
-
-  // Derives all the images and adds them to the reduced result
-  images.forEach((i) => {
-    var imageUrl = completeImageUrl($(i).attr('src') || '');
-
-    // Dedupe images and only add one of each
-    if (imageUrl && $.inArray(imageUrl, reduced.results.allImages) === -1) {
-      reduced.results.allImages.push(imageUrl);
-    }
-  });
-
-  console.log(reduced);
-
   // Only send gotten fonts and colors upon message from popup.js
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.get === "pageData") {
+      // Get fonts and colors on page load
+      const elements = $.makeArray($('body *').not('script, link, style'));
+      const images = $.makeArray($('body img'));
+
+      let reduced = reduceColorsAndFonts(elements);
+
+      // Sort fonts in alphabetical order
+      reduced.results.allFonts.sort((a, b) => {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+      });
+
+      // Derives all the images and adds them to the reduced result
+      images.forEach((i) => {
+        var imageUrl = completeImageUrl($(i).attr('src') || '');
+
+        // Dedupe images and only add one of each
+        if (imageUrl && $.inArray(imageUrl, reduced.results.allImages) === -1) {
+          reduced.results.allImages.push(imageUrl);
+        }
+      });
+
+      console.log(reduced);
       // Send data to popup.js
       sendResponse({
         colors: reduced.results.allColors,
