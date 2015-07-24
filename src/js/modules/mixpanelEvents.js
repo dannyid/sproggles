f7937e7e72ac4c6aee5a75a285337288face875f
Danny Shekhtman <danielshekhtman@gmail.com>
2015-07-24 14:36:25 -0400

    Add prod checks to all events

--- src/js/modules/mixpanelEvents.js
+++ src/js/modules/mixpanelEvents.js
@@ -1,4 +1,5 @@
 import {get as $get} from 'jquery';
+import prod from './productionCheck';
 import {MIXPANEL_EVENT_URL, MIXPANEL_TOKEN} from './constants';
 
 function createEventJSON(eventName) {
@@ -12,49 +13,67 @@ function createEventJSON(eventName) {
 
 export function tabClicked(tabId) {
   const eventData = createEventJSON(`Clicked ${tabId} tab`);
-
-  $get(MIXPANEL_EVENT_URL + eventData, function(data) {
-    console.log('Mixpanel click event fired: ', data);
-  });
+  if (prod()) {
+    $get(MIXPANEL_EVENT_URL + eventData, function(data) {
+      console.log('Mixpanel click event fired: ', data);
+    });
+  } else {
+    console.log('In QA so no event fired');
+  }
 }
 
 export function popupOpened() {
   const eventData = createEventJSON(`Popup opened`);
-
-  $get(MIXPANEL_EVENT_URL + eventData, function(data) {
-    console.log('Mixpanel popup open event fired: ', data);
-  });
+  if (prod()) {
+    $get(MIXPANEL_EVENT_URL + eventData, function(data) {
+      console.log('Mixpanel popup open event fired: ', data);
+    });
+  } else {
+    console.log('In QA so no event fired');
+  }
 }
 
 export function popupFailed() {
   const eventData = createEventJSON(`Popup failed`);
-
-  $get(MIXPANEL_EVENT_URL + eventData, function(data) {
-    console.log('Mixpanel popup failed event fired: ', data);
-  });
+  if (prod()) {
+    $get(MIXPANEL_EVENT_URL + eventData, function(data) {
+      console.log('Mixpanel popup failed event fired: ', data);
+    });
+  } else {
+    console.log('In QA so no event fired');
+  }
 }
 
 export function darkThemeClicked() {
   const eventData = createEventJSON(`Dark theme clicked`);
-
-  $get(MIXPANEL_EVENT_URL + eventData, function(data) {
-    console.log('Mixpanel dark theme click event fired: ', data);
-  });
+  if (prod()) {
+    $get(MIXPANEL_EVENT_URL + eventData, function(data) {
+      console.log('Mixpanel dark theme click event fired: ', data);
+    });
+  } else {
+    console.log('In QA so no event fired');
+  }
 }
 
 export function feedbackButtonClicked() {
   const eventData = createEventJSON(`Feedback button clicked`);
-
-  $get(MIXPANEL_EVENT_URL + eventData, function(data) {
-    console.log('Mixpanel feedback button click event fired: ', data);
-  });
+  if (prod()) {
+    $get(MIXPANEL_EVENT_URL + eventData, function(data) {
+      console.log('Mixpanel feedback button click event fired: ', data);
+    });
+  } else {
+    console.log('In QA so no event fired');
+  }
 }
 
 export function colorCopied() {
   const eventData = createEventJSON(`Color copied to clipboard`);
-
-  $get(MIXPANEL_EVENT_URL + eventData, function(data) {
-    console.log('Mixpanel color copied to clipboard event fired: ', data);
-  });
+  if (prod()) {
+    $get(MIXPANEL_EVENT_URL + eventData, function(data) {
+      console.log('Mixpanel color copied to clipboard event fired: ', data);
+    });
+  } else {
+    console.log('In QA so no event fired');
+  }
 }
 
