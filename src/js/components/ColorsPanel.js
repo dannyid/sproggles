eb5c5cb4935947252008a63a4454bf22537eb857
Danny Shekhtman <danielshekhtman@gmail.com>
2015-09-15 14:26:49 -0400

    Move styles from reactify branch to normalize panel

--- src/js/components/ColorsPanel.js
+++ src/js/components/ColorsPanel.js
@@ -7,101 +7,110 @@ const important = '!important';
 // const colors = ['#CCC', '#999', '#333', '#CCC', '#CCC'];
 
 const ColorsPanel = React.createClass({
-  getInitialState: function() {
-    return {
-      open: true
-    };
-  },
-
   getDefaultProps: function() {
     return {
-      colorsPanelStyle: {
-        background: `white ${important}`,
-        // border: `1px solid #CCC ${important}`,
-        boxShadow: /*0 -1px 0 #e5e5e5,*/ `0 0 2px rgba(0,0,0,.12), 0 2px 4px rgba(0,0,0,.24) ${important}`,
-        fontFamily: `Helvetica ${important}`,
-        fontSize: `16px ${important}`,
-        height: `400px ${important}`,
-        left: `10px ${important}`,
-        padding: `0 ${important}`,
-        opacity: `0.98 ${important}`,
-        position: `fixed ${important}`,
-        textAlign: `left ${important}`,
-        top: `10px ${important}`,
-        width: `250px ${important}`,
-        zIndex: `9999999999999999 ${important}`
-      },
+      styles: {
+        colorsPanelStyle: {
+          base: {
+            background: `white`,
+            // border: `1px solid #CCC`,
+            boxShadow: /*0 -1px 0 #e5e5e5,*/ `0 0 2px rgba(0,0,0,.12), 0 2px 4px rgba(0,0,0,.24)`,
+            fontFamily: `Helvetica`,
+            fontSize: 16,
+            left: 10,
+            padding: 0,
+            opacity: 0.98,
+            overflow: `hidden`,
+            position: `fixed`,
+            top: 10,
+            width: 250,
+            zIndex: `9999999999999999`
+          },
 
-      panelToolbarStyle: {
-        backgroundColor: `dodgerblue ${important}`,
-        border: `1px solid hsl(210, 100%, 48%) ${important}`,
-        color: `white ${important}`,
-        display: `table-cell ${important}`,
-        fontSize: `20px ${important}`,
-        fontWeight: `400 ${important}`,
-        height: `22px ${important}`,
-        padding: `5px ${important}`,
-        verticalAlign: `middle ${important}`,
-        width: `250px ${important}`
-      },
+          opened: {
+            height: 400
+          },
 
-      closeButtonStyle: {
-        color: `white ${important}`,
-        fontFamily: `Helvetica ${important}`,
-        fontSize: `18px ${important}`,
-        position: `absolute ${important}`,
-        right: `12px ${important}`,
-        textDecoration: `none ${important}`,
-        transform: `scaleX(1.3) ${important}`,
-        top: `6px ${important}`
-      },
+          closed: {
+            height: 35
+          }
+        },
 
-      panelTextStyle: {
-        display: `block ${important}`,
-        fontFamily: `Helvetica ${important}`,
-        fontSize: `14px ${important}`,
-        margin: `0 ${important}`,
-        padding: `5px ${important}`
+        closeButtonStyle: {
+          color: `white`,
+          fontFamily: `Helvetica`,
+          fontSize: 18,
+          position: `absolute`,
+          right: 12,
+          textDecoration: `none`,
+          transform: `scaleX(1.3)`,
+          top: 6
+        },
+
+        panelToolbarStyle: {
+          backgroundColor: `dodgerblue`,
+          border: `1px solid hsl(210, 100%, 48%)`,
+          color: `white`,
+          display: `table-cell`,
+          fontSize: `20px`,
+          fontWeight: `400`,
+          height: `22px`,
+          padding: `5px`,
+          verticalAlign: `middle`,
+          width: `250px`
+        },
+
+        panelTextStyle: {
+          display: `block`,
+          fontFamily: `Helvetica`,
+          fontSize: `14px`,
+          margin: `0`,
+          padding: `5px`
+        }
       }
     };
   },
 
-  closePanel: function(e) {
+  getInitialState: function() {
+    return {
+      isOpen: true
+    };
+  },
+
+  togglePanel: function(e) {
     e.preventDefault();
 
     this.setState({
-      open: false
+      isOpen: !this.state.isOpen
     });
   },
 
   render: function() {
-    if (this.state.open) {
-      return (
-        <div className="colorsPanel" style={this.props.colorsPanelStyle}>
-          <div className="panelToolbar" style={this.props.panelToolbarStyle}>
-            <span>
-              Colors
-            </span>
-            <a className="closeButton" style={this.props.closeButtonStyle} onClick={this.closePanel} href>
-              x
-            </a>
-          </div>
+    const openState =
+      this.state.isOpen
+      ? this.props.styles.colorsPanelStyle.opened
+      : this.props.styles.colorsPanelStyle.closed;
 
-          <p style={this.props.panelTextStyle}>
-            Hello, I am a React component embedded on the screen!
-          </p>
-          {this.props.colors.map(function(color){
-            return (
-              <ColorSquare color={color}/>
-            );
-          })}
+    return (
+      <div className="colorsPanel" style={preNormalize(this.props.styles.colorsPanelStyle.base, openState)}>
+        <div className="panelToolbar" style={preNormalize(this.props.styles.panelToolbarStyle)}>
+          <span>
+            Colors
+          </span>
+          <a className="closeButton" style={preNormalize(this.props.styles.closeButtonStyle)} onClick={this.togglePanel} href>
+            x
+          </a>
         </div>
-      );
-    } else {
-      return (
-        <span></span>
-      );
-    }
+        <p style={preNormalize(this.props.styles.panelTextStyle)}>
+          Hello, I am a React component embedded on the screen!
+        </p>
+        {this.props.colors.map(function(color){
+          return (
+            <ColorSquare color={color}/>
+          );
+        })}
+      </div>
+    );
   }
 });
 
