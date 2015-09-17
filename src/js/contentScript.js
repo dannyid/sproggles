e5613a3e7d57db5d35250ef16128d81964fc39d6
Danny Shekhtman <danielshekhtman@gmail.com>
2015-09-17 15:50:27 -0400

    Make browserAction toggle app. Fixes #46

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -2,26 +2,30 @@ import React from 'react';
 import $ from 'jquery';
 import App from './components/App.js';
 
+const renderApp = (appVisible) => {
+  React.render(
+    <App visible={appVisible}/>,
+    document.getElementById('sproggles-app-container')
+  );
+};
+
 $(() => {
-  let appVisible = true;
+  let appVisible = false;
   // Inject the App but only if it's not already injected
   if (document.getElementById('sproggles-app-container') === null) {
     const app = document.createElement('div');
     app.id = 'sproggles-app-container';
     document.body.insertBefore(app, document.body.firstChild);
-
-    React.render(
-      <App visible={appVisible}/>,
-      document.getElementById('sproggles-app-container')
-    );
+    renderApp(appVisible);
   }
 
-  // Only send gotten fonts and colors upon message from popup.js
+  // Receive browserAction click event from background script and toggle app
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log('Content Script hears click.');
     if (request.action === "toggleApp") {
       console.log('The click is a "toggleApp" event.');
       appVisible = !appVisible;
+      renderApp(appVisible);
     }
   });
 });
