296400475f67ef41f5bfa69a8e358f97c9054f13
Danny Shekhtman <danielshekhtman@gmail.com>
2015-09-22 13:49:53 -0400

    Import only getJSON instead of entire jQuery library in getVolume

--- src/js/modules/getVolume.js
+++ src/js/modules/getVolume.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+import {getJSON as $getJSON} from 'jquery';
 import {SEARCH_VOLUME_URL} from './constants';
 import Q from 'q';
 
@@ -8,26 +8,6 @@ export const getSingleKeywordVolume = keyword => {
   const encodedKeyword = encodeURIComponent(keyword);
   console.log(`Getting Volume data for keyword: ${decodeURIComponent(encodedKeyword)}`);
 
-  return Q($.getJSON(`${SEARCH_VOLUME_URL}/${encodedKeyword}`))
+  return Q($getJSON(`${SEARCH_VOLUME_URL}/${encodedKeyword}`))
   .then(response => response.volume);
 };
-
-// export const getVolumes = ({keywords, url}) => {
-//   return Q.all(keywords.map(getSingleKeywordVolume))
-//   .then(keywordsWithVolume => {
-//     const deferred = Q.defer();
-
-//     const keywordsAboveVolumeThreshold = keywordsWithVolume.filter(keywordWithVolume => {
-//       return keywordWithVolume.volume >= volumeThreshold;
-//     }).sort((a, b) => {
-//       return b.volume - a.volume; /* sort in descending order by volume */
-//     });
-
-//     deferred.resolve({
-//       keywords: keywordsAboveVolumeThreshold,
-//       url
-//     });
-
-//     return deferred.promise;
-//   });
-// };
