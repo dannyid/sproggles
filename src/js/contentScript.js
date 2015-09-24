e8e25547d4053b9f78eb8a63b6ac0b597a457597
Danny Shekhtman <danielshekhtman@gmail.com>
2015-09-24 00:02:28 -0400

    Clean up App mounting and unmounting

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -2,23 +2,32 @@ import React from 'react';
 import {domReady} from './modules/utils'; // Replacement for jQuery's ready function
 import App from './components/App.js';
 
+function mountApp() {
+  console.log('mountApp');
+  React.render(
+    <App />,
+    document.getElementById('sproggles-app-container')
+  );
+  return true;
+}
+
+function unmountApp() {
+  console.log('unmountApp');
+  const appContainer = document.getElementById('sproggles-app-container');
+  // The below returns false is there's no React component mounted
+  return React.unmountComponentAtNode(appContainer);
+}
+
 domReady(() => {
-  let appVisible = false;
+  // Inject app container div
+  const app = document.createElement('div');
+  app.id = 'sproggles-app-container';
+  document.body.insertBefore(app, document.body.firstChild);
+
   // Receive browserAction click event from background script and toggle app
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "toggleApp") {
-      if (appVisible) {
-        document.getElementById('sproggles-app-container').remove();
-      } else {
-        const app = document.createElement('div');
-        app.id = 'sproggles-app-container';
-        document.body.insertBefore(app, document.body.firstChild);
-        React.render(
-          <App />,
-          document.getElementById('sproggles-app-container')
-        );
-      }
-      appVisible = !appVisible;
+      unmountApp() || mountApp();
     }
   });
 });
