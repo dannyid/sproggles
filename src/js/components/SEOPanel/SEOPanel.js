0b9bb94061bab44ee6240590d82c692b6e519713
Danny Shekhtman <danielshekhtman@gmail.com>
2015-09-24 01:50:12 -0400

    Add search result and social share count callbacks as props to children

--- src/js/components/SEOPanel/SEOPanel.js
+++ src/js/components/SEOPanel/SEOPanel.js
@@ -7,15 +7,24 @@ import KeywordInfo from './KeywordInfo';
 
 const SEOPanel = React.createClass({
   render: function() {
-    const {data, title, toggle, isOpen, getKeywordInfo} = this.props;
+    const {
+      data,
+      title,
+      isOpen,
+      toggle,
+      getResult,
+      getSocialCounts,
+      getKeywordInfo
+    } = this.props;
+
     const {resultJson, shareCounts, keywordInfo} = data;
 
     return (
-      <PanelContainer title={title} toggle={toggle} isOpen={isOpen}>
+      <PanelContainer title={title} isOpen={isOpen} toggle={toggle}>
         <Heading text="Google result" subtext="(click to see on Google)" />
-        <SearchResult resultJson={resultJson} />
+        <SearchResult resultJson={resultJson} getResult={getResult} />
         <Heading text="Social Share Counts" subtext="" />
-        <SocialShareCounts shareCounts={shareCounts} />
+        <SocialShareCounts shareCounts={shareCounts} getSocialCounts={getSocialCounts} />
         <Heading text="Real-time Keyword Research" subtext="" />
         <KeywordInfo keywordInfo={keywordInfo} getKeywordInfo={getKeywordInfo}/>
       </PanelContainer>
