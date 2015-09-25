518883827afa8bd8872d10569fd9e613e1323d3d
Danny Shekhtman <danielshekhtman@gmail.com>
2015-09-25 12:33:17 -0400

    Add comment about Chrome Storage quota limits

--- src/js/modules/chromeStorage.js
+++ src/js/modules/chromeStorage.js
@@ -1,5 +1,17 @@
 import Q from 'q';
 
+/***
+ *
+ * Chrome Storage has quota limts - https://developer.chrome.com/extensions/storage#property-sync
+ *
+ * .local has about 50 times more space than .sync but also doesn't sync Google
+ * users won't be able to get those settings on another computer
+ *
+ * I switched to .local when saving state on Buzzfeed went over the individual quota limit of ~8k
+ * There seems not to be a quota limit per item for storing in .local
+ *
+ */
+
 export const get = (item) => {
   const deferred = Q.defer();
 
