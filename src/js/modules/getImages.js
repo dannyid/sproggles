741324dd1ac18f7a64f895d906dc095e8c145bc1
Danny Shekhtman <danielshekhtman@gmail.com>
2015-10-06 18:33:16 -0400

    Prevent background-image gradients from showing up in images

--- src/js/modules/getImages.js
+++ src/js/modules/getImages.js
@@ -7,7 +7,13 @@ export default domElements => {
     .map(e => window.getComputedStyle(e).backgroundImage // Only grab elements with background-image
     .slice(4, -1)); // remove url() wrapping
 
-  const filteredImages = [...imageElementUrls, ...bgImageUrls].filter(e => e !== '' && e.indexOf('chrome-extension://') === -1); // Exclude images from chrome-extensions
+  const filteredImages = [...imageElementUrls, ...bgImageUrls].filter(e => {
+    return (
+      e !== '' &&
+      e.indexOf('chrome-extension://') === -1 && // Prevent chrome-extension images
+      e.indexOf('gradient') === -1 // Prevent background-images that are gradients
+    );
+  });
 
   const dedupedImages = new Set(filteredImages);
 
